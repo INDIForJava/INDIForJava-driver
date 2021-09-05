320cd150b6f0c6225ea3b04cc1e5736a46ed8300
Marco Cipriani <marco.cipriani.01@gmail.com>
2021-09-05 11:43:42 +0200

    Gradle 7.1 and Kotlin KTS

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("idea")
+}
+
+group = "org.indilib.i4j"
+description = "INDIForJava-driver"
+version = "2.0.2"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    api("com.github.INDIForJava:INDIForJava-core:2.0.2")
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        from("META-INF/MANIFEST.MF")
+    }
+}
+
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set(project.name)
+                description.set("INDIForJava is a set of libraries to implement clients and servers that follow the INDI protocol, designed to operate astronomical instrumentation.")
+                url.set("https://github.com/INDIForJava/INDIForJava-driver")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("marcocipriani01")
+                        name.set("Marco Cipriani")
+                        email.set("marco.cipriani.01@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/INDIForJava/INDIForJava-driver.git")
+                    developerConnection.set("scm:git:ssh://github.com/INDIForJava/INDIForJava-driver.git")
+                    url.set("https://github.com/INDIForJava/INDIForJava-driver")
+                }
+            }
+        }
+    }
+}
